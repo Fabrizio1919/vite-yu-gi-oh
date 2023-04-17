e7108e15014a3c0bf39a06d2aebec9491678f40a
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-17 17:09:07 +0200

    Add name and image in appMain

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,67 @@
 import AppHeader from "./components/AppHeader.vue"
 import AppMain from "./components/AppMain.vue"
 import AppFooter from "./components/AppFooter.vue"
+import axios from 'axios'
 export default {
   components: {
     AppHeader,
     AppMain,
     AppFooter
+  },
+  data() {
+    return {
+      API_URL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=15',
+      cards: [],
+
+    }
+  },
+  methods: {
+    calApi(url) {
+      axios
+        .get(url)
+        .then(response => {
+
+          this.saluto = "Ciao";
+
+          console.log(response.data.data);
+          this.cards = response.data.data;
+        })
+        .catch(err => {
+          console.error(err.message)
+        })
+    }
+  },
+  mounted() {
+    this.calApi(this.API_URL)
   }
-  
+
 }
 
 </script>
 
 <template>
-  <header>
-
-  </header>
+  <AppHeader></AppHeader>
 
   <main>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5">
+        <div class="col colTotal" v-for="card in cards">
+          <div class="cards">
+            <img class="imgCards" :src="card.card_images[0].image_url">
+            <div class="textCard">
+              <h6>{{ card.name }}</h6>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
 
   </main>
 
   <footer>
 
   </footer>
-  
 </template>
 
-<style>
-
-</style>
+<style></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,14 @@ export default {
 </script>
 
 <template>
-    
+    <header>
+        <div class="logo d-flex">
+            <img class="imgLogo" src="../assets/img/yu-gi-oh-logo.jpg" alt="">
+            <div class="textlogo">
+                <h2>Yu-Gi-Oh Api</h2>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style>
-
-</style>
+<style></style>
